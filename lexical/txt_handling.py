9ca0947e66357e2709ef85eb40953e472fb9e964
Tara Chow <tarachow2@gmail.com>
2024-01-31 00:38:42 -0500

    add folders for files and lexical

--- lexical/txt_handling.py
+++ lexical/txt_handling.py
@@ -3,7 +3,7 @@ import re
 # reading and saving keywords from txt file into 2d array
 def read_keywords ():
     keywords = []
-    file = open('keywords.txt', 'r')
+    file = open('files/keywords.txt', 'r')
     content=file.readlines()
     for con in content:
         res = list(filter(None, re.split(',|:|\n| ', con)))
@@ -14,11 +14,9 @@ def read_keywords ():
 # reading and saving transition table from txt file to 2d array 
 def read_transition_table():
     trans_table = []
-    file = open('transition_table.txt', 'r')
+    file = open('files/transition_table.txt', 'r')
     table_content=file.readlines()
     for con in table_content:
         trans_table.append(con.split())
-    # print(TRANS_TABLE[1])
-    # print(TRANS_TABLE[1][3])
     file.close()
     return trans_table

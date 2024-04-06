b0a2a2b9a1978acd3a622eaee9d26e1e5db15ef6
Tara Chow <tarachow2@gmail.com>
2024-04-06 15:01:53 -0400

    setup function

--- generate/code_gen.py
+++ generate/code_gen.py
@@ -2,7 +2,16 @@ from analyser import analyse
 
 rootTable, program, semErr = analyse()
 
-if semErr:
-    print("Syntax errors, cannot proceed with semantic analysis.")
-else:
-    print("all good!")
+def generate():
+    if semErr:
+        print("Syntax errors, cannot proceed with semantic analysis.")
+        return
+    
+    for key, var in rootTable.functions.items():
+        print('''
+key: {}
+{}'''.format(key, var))
+    
+
+#* MAIN
+generate()

ef183718f3b6ecba04fc7c731352cf6c57b3ed69
Tara Chow <tarachow2@gmail.com>
2024-04-13 05:45:59 -0400

    completed code gen yay

--- generate/code_gen.py
+++ generate/code_gen.py
@@ -4,58 +4,75 @@ import nodes
 rootTable, program, semErr = analyse()
 code = [] #3TAC generated code - a list for now cause idk
 
-# track counts for labels, registers and temp values
-labelCount = 1
-regCount = 1
+# counts for tracking
 tempCount = 1
-funcHasReturn = 0
+boolLabelCount = 1
+orLabelCount = 1
+andLabelCount = 1
+ifCount = 1
+localTempCount = 1
+compCount = 1
+    
+# map of operators to compare
+compOps = {
+    '>': 'BGT',
+    '<': 'BLT',
+    '>=': 'BGE',
+    '<=': 'BLE',
+    '<>': 'BNE',
+    '==': 'BEQ'
+}
 
 # track function return registers
 funcRegs = {}
 
 def generate():
     if semErr:
-            # write error to error output file
-            if isinstance(semErr, str):
-                print(semErr)
-                outputFile(semErr)
-                return None, semErr
-            else:
-                outputFile("Semantic errors, cannot proceed with intermediate code generation.")
-                print("Semantic errors, cannot proceed with intermediate code generation.")
-            return None, "Semantic errors, cannot proceed with intermediate code generation."
-    
-    print(rootTable)    
-    # print(program)
+        # write error to error output file
+        if isinstance(semErr, str):
+            print(semErr)
+            outputFile(semErr)
+            return None, semErr
+        else:
+            outputFile("Semantic errors, cannot proceed with intermediate code generation.")
+            print("Semantic errors, cannot proceed with intermediate code generation.")
+        return None, "Semantic errors, cannot proceed with intermediate code generation."
 
-    # generate function declarations
-    if len(rootTable.functions) > 0:
-        code.append("B main")
-        # go through functions
-        for func in program.fdecls:
-            # print(func)
-            funcTable = rootTable.functions[func.fname[1]]
-            code.append(func.fname[1] + ":")
-            code.append("begin " + str(len(funcTable.table.variables) * 4))
-            # double check if I just do these pushes every time or not ..
-            code.append("push LR")
-            # go through params
-            paramPoint = 4
-            for param in func.params:
-                paramPoint += 4
-                code.append("{} = [sp + {}]".format(param.var.id[1], paramPoint))
-            genStmts(func.stmtSeq)
-            if funcHasReturn > 0:
-                funcRegs[func.fname[1]] = ["r{}".format(i + 1) for i in range(funcHasReturn)]
-            #! is this supposed to always be at the end of a function??
-            code.append("pop {PC}")
+    code.append("B main")
+
+    # functions
+    global localTempCount
+    for func in program.fdecls:
+        funcTable = rootTable.functions[func.fname[1]]
+        localTempCount = 0
+
+        code.append(func.fname[1] + ":")
+        code.append("push LR")
+        funcBeginIndex = len(code)
+        code.append("begin temp")
+
+        # go through params
+        paramPoint = 4
+        for param in func.params:
+            paramPoint += 4
+            code.append("{} = [sp + {}]".format(param.var.id[1], paramPoint))
+        genStmts(func.stmtSeq)
+        
+        memorySize = (len(funcTable.table.variables) * 4) + localTempCount*4
+        code[funcBeginIndex] = "begin {}".format(memorySize)
+        code.append("pop {PC}")
     
     code.append("main:")
-    #! why is this begin 48??????
-    code.append("begin " + str((len(rootTable.variables) + len(program.stmtSeq)) * 4))
+    mainBeginIndex = len(code)
+    code.append("begin temp main")
+    localTempCount = 0
 
     # generate statements
     genStmts(program.stmtSeq)
+    memorySize = (len(rootTable.variables) * 4) + localTempCount*4
+    code[mainBeginIndex] = "begin {}".format(memorySize)
+
+    # wrap up
     printFinal(code)
     outputFile(code)
     return code
@@ -66,56 +83,34 @@ params:
 return: -
 '''
 def genStmts(stmtSeq):
-    global labelCount, regCount, funcHasReturn
-    for index, stmt in enumerate(stmtSeq):
+    for stmt in stmtSeq:
         if isinstance(stmt, nodes.assignStmtNode):
             first = stmt.var.id[1]
             second = genExpr(stmt.expr)
-            print("ASSIGN STMT NODE")
             code.append("{} = {}".format(first, second))
         elif isinstance(stmt, nodes.ifStmtNode):
-            ifStr = ""
-            bcomp = stmt.bexpr.bterm.bfactor.bcomp
-            #TODO: make this a map?
-            if bcomp.comp[1] == ">":
-                ifStr += "BGT"
-            elif bcomp.comp[1] == "<":
-                ifStr += "BLT"
-            elif bcomp.comp[1] == ">=":
-                ifStr += "BGE"
-            elif bcomp.comp[1] == "<=":
-                ifStr += "BLE"
-            elif bcomp.comp[1] == "<>":
-                ifStr += "BNE"
-            elif bcomp.comp[1] == "==":
-                ifStr += "BEQ"
-            ifStr += " {}, {}, label{}".format(
-                bcomp.expr1.term.factor.node.id[1],
-                bcomp.expr2.term.factor.node.id[1],
-                labelCount
-            )
-            code.append(ifStr)
-            code.append("label{}:".format(labelCount))
-            labelCount += 1
+            genBExpr(stmt.bexpr, "", "", False)
+            ifLabel, elseLabel, endIfLabel = getPrevIfLabel()
+            code.append("{}:".format(ifLabel))
             genStmts(stmt.stmtSeq)
+            code.append("b {}".format(endIfLabel))
+            code.append("{}:".format(elseLabel))
             if len(stmt.elseStmtSeq) > 0:
-                code.append("B label{}".format(labelCount + 1))
-                code.append("label{}:".format(labelCount))
-                labelCount += 1
                 genStmts(stmt.elseStmtSeq)
-            if len(stmtSeq)-1 != index:
-                code.append("label{}:".format(labelCount))
-                labelCount += 1
+            code.append("{}:".format(endIfLabel))
         elif isinstance(stmt, nodes.whileStmtNode):
-            # TODO: while statement
-            print(stmt)
-            print("WHILE STATEMENT")
+            genBExpr(stmt.bexpr, "", "", False)
+            ifLabel, elseLabel, endIfLabel = getPrevIfLabel()
+            code.append("{}:".format(ifLabel))
+            genStmts(stmtSeq)
+            boolLabel = getBoolLabel()
+            code.append("b {}".format(boolLabel))
+            code.append("{}:".format(elseLabel))
+            code.append("{}:".format(endIfLabel))
         elif isinstance(stmt, nodes.builtStmt):
             if stmt.type[1] == "return":
                 second = genExpr(stmt.expr)
-                code.append("r{} = {}".format(regCount, second))
-                regCount += 1
-                funcHasReturn += 1
+                code.append("r1 = {}".format(second))
             elif stmt.type[1] == "print":
                 second = genExpr(stmt.expr)
                 code.append("print {}".format(second))
@@ -124,131 +119,273 @@ def genStmts(stmtSeq):
 params:
 - expr (exprNode)
 return: 
-- exprLine (string): line of code
+- first (string): term
 '''
 def genExpr(expr):
-    print("GEN EXPR")
-    print(expr)
-    exprLine = ""
-    exprLine += genTerm(expr.term)
+    first = genTerm(expr.term)
     if expr.exprEnd is not None:
-        #! what is supposed to happen in this case? 
-        exprLine += genExprEnd(expr.exprEnd)
-    print(exprLine)
-    return exprLine
+        termQ, opQ = genExprEnd(expr.exprEnd, [], [])
+        for i in range(len(termQ)):
+            temp = newTempVar()
+            op = opQ[i]
+            second = termQ[i]
+            code.append("{} = {} {} {}".format(temp, first, op, second))
+    return first
 
 '''
 #* generate exprEnd
 params:
 - expr (exprEndNode)
+- termQ (queue): track terms in order
+- opQ (queue): track ops in order
 return:
-- exprLine ()
+- termQ (queue): track terms in order
+- opQ (queue): track ops in order
 '''
-def genExprEnd(expr):
-    global tempCount
-    print("GEN EXPREND")
-    print(expr)
+def genExprEnd(expr, termQ, opQ):
     term = genTerm(expr.term)
-    print("{} {}".format(expr.op[1], term))
+    termQ.append(term)
+    opQ.append(expr.op[1])
     if expr.exprE is not None:
-        print("DO ANOTHER EXPREND")
-        # TODO: figure out what to do here
-        # print(expr.exprE)
-        # genExprEnd(expr.exprE)
-        # print(genExprEnd(expr.exprE))
-        # print("t{} = t{} {} {}".format(tempCount, tempCount-1, expr.op[1], term))
-        # code.append("t{} = t{} {} {}".format(tempCount, tempCount-1, expr.op[1], term))
-        # tempCount += 1
-        # return genExprEnd(expr.exprE)
-        # return (" {} t{}".format(expr.op[1], tempCount-1))
-    return (" {} {}".format(expr.op[1], term))
+        return genExprEnd(expr.exprEnd, termQ, opQ)
+    return termQ, opQ
 
 '''
 #* generate term
 params:
 - term (termnode)
 return:
-- termLine ()
+- term (string)
 '''
 def genTerm(term):
-    print("GEN TERM")
-    print(term)
-    termLine = ""
-    termLine += genFactor(term.factor)
+    first = genFactor(term.factor)
     if term.termEnd is not None:
-        print(term.termEnd)
-        termLine += genTermEnd(term.termEnd)
-    return termLine
-
-'''
-#* generate termEnd
-params:
-- term (termEndNode)
-return:
-- termLine ()
-'''
-def genTermEnd(term):
-    global tempCount
-    print("GEN TERMEND")
-    print(term)
-    factor = genFactor(term.factor)
-    print("{} {}".format(term.op[1], factor))
-    if term.termE is not None:
-        print("DO ANOTHER EXPREND")
-        # TODO: figure out what to do here
-        # print(expr.exprE)
-        # genExprEnd(expr.exprE)
-        # print(genExprEnd(expr.exprE))
-        # print("t{} = t{} {} {}".format(tempCount, tempCount-1, expr.op[1], term))
-        # code.append("t{} = t{} {} {}".format(tempCount, tempCount-1, expr.op[1], term))
-        # tempCount += 1
-        # return genExprEnd(expr.exprE)
-        # return (" {} t{}".format(expr.op[1], tempCount-1))
-    return (" {} {}".format(term.op[1], factor))
+        temp = newTempVar()
+        op = term.termEnd.op[1]
+        second = genFactor(term.termEnd.factor)
+        code.append("{} = {} {} {}".format(temp, first, op, second))
+        return temp
+    return first
 
 '''
 #* generate factor
 params:
 - factor (factorNode)
 return:
-- factorLine (string)
+- term (string)
 '''
 def genFactor(factor):
-    global tempCount
-    print("GEN FACTOR")
-    print(factor)
     if factor.type == "number":
-        code.append("t{} = {}".format(tempCount, factor.node[1]))
-        tempCount += 1
-        return("t{}".format(tempCount-1))
+        temp = newTempVar()
+        code.append("{} = {}".format(temp, factor.node[1]))
+        return(temp)
     elif factor.type == "expr":
         return genExpr(factor.node)
     elif factor.type == "funcCall":
         fCallNode = factor.node
-        print("GEN FACTOR FCALLNODE")
-        print(fCallNode)
-        funcParams = []
-        for index in range(len(fCallNode.exprSeq)):
-            print("FUNC CALL PARAM")
-            print(fCallNode.exprSeq[index])
-            param = genExpr(fCallNode.exprSeq[index])
-            funcParams.append(param)
-            code.append("push {}".format(param))
+        args = []
+        for expr in fCallNode.exprSeq:
+            temp = genExpr(expr)
+            args.append(temp)
+        # for arg in args:
+        for i in range(len(args) - 1, -1, -1):
+            code.append("push {}".format(args[i]))
         code.append("BL {}".format(fCallNode.fname))
-        funcTemps = []
-        for reg in funcRegs[fCallNode.fname]:
-            funcTemps.append("t{}".format(tempCount))
-            code.append("t{} = {}".format(tempCount, reg))
-            tempCount += 1
-        for param in funcParams:
-            code.append("pop {}".format(param))
-        return(funcTemps[0])
+        temp = newTempVar()
+        code.append("{} = r1".format(temp))
+        for arg in args:
+            code.append("pop {}".format(arg))
+        return temp
     elif factor.type == "var":
         return(factor.node.id[1])
 
-# TODO: statements: while loops
-# TODO: bexpr, bterm, bfactor
-# TODO: figure out more than 1 exprEnd or termEnd (z=2+3+4 or z=2*3*4)
+'''
+#* generate bexpr
+params:
+- expr (bexprNode)
+- exitLabel (string): label for exit/false
+- destLabel (string): label for next destination/true
+- negate (bool): if there's a not
+return: -
+'''
+def genBExpr(expr, exitLabel, destLabel, negate):
+    if exitLabel != "" and destLabel != "":
+        _exit = exitLabel
+        _dest = destLabel
+    else:
+        ifLabel, elseLabel, _ = getIfLabel()
+        if expr.bexprEnd is not None:
+            _exit = getORLabel()
+        else:
+            _exit = elseLabel
+        _dest = ifLabel
+    boolLabel = getBoolLabel()
+    code.append("{}:".format(boolLabel))
+    genBterm(expr.bterm, _exit, _dest, negate)
+    if expr.bexprEnd is not None:
+        code.append("{}:".format(_exit))
+        genBexprEnd(expr.bexprEnd, elseLabel, ifLabel, negate)
+
+'''
+#* generate bexprEnd
+params:
+- expr (bexprEndNode)
+- exitLabel (string): label for exit/false
+- destLabel (string): label for next destination/true
+- negate (bool): if there's a not
+return: -
+'''
+def genBexprEnd(expr, exitLabel, destLabel, negate):
+    if expr.bexprEnd is not None:
+        orLabel = getORLabel()
+    else:
+        orLabel = exitLabel
+    genBterm(expr.bterm, orLabel, destLabel, negate)
+    if expr.bexprEnd is not None:
+        code.append("{}:".format(orLabel))
+        genBexprEnd(expr.bexprEnd, exitLabel, destLabel, negate)
+
+'''
+#* generate bterm
+params:
+- term (btermnode)
+- exitLabel (string): label for exit/false
+- destLabel (string): label for next destination/true
+- negate (bool): if there's a not
+return: -
+'''
+def genBterm(term, exitLabel, destLabel, negate):
+    if term.btermEnd is not None:
+        andLabel = getANDLabel()
+    else:
+        andLabel = destLabel
+    genBfactor(term.bfactor, exitLabel, andLabel, negate)
+    if term.btermEnd is not None:
+        code.append("{}:".format(andLabel))
+        genBterm(term.btermEnd, exitLabel, destLabel, negate)
+
+'''
+#* generate bfactor
+params:
+- factor (bfactorNode)
+- exitLabel (string): label for exit/false
+- destLabel (string): label for next destination/true
+- negate (bool): if there's a not
+return: -
+'''
+def genBfactor(factor, exitLabel, destLabel, negate):
+    if factor.bcomp is not None:
+        first = genExpr(factor.bcomp.expr1)
+        second = genExpr(factor.bcomp.expr2)
+        if negate:
+            third = exitLabel
+            fourth = destLabel
+        else:
+            third = destLabel
+            fourth = exitLabel
+        code.append("{} {}, {}, {}".format(
+            compOps[factor.bcomp.comp[1]],
+            first,
+            second,
+            third,
+        ))
+        code.append("b {}".format(fourth))
+    elif factor.bfactor is not None:
+        _negate = (negate != factor.negate)
+        genBfactor(factor.bfactor, exitLabel, destLabel, _negate)
+    elif factor.bexpr is not None:
+        _negate = (negate != factor.negate)
+        genBExpr(factor.bexpr, exitLabel, destLabel, _negate)
+
+'''
+#* get or label
+params: -
+return: or label
+'''
+def getORLabel():
+    global orLabelCount
+    orLab = "or_{}".format(orLabelCount)
+    orLabelCount += 1
+    return orLab
+
+'''
+#* get and label
+params: -
+return: and label
+'''
+def getANDLabel():
+    global andLabelCount
+    andLab = "and_{}".format(andLabelCount)
+    andLabelCount += 1
+    return andLab
+
+'''
+#* get if labels
+params: -
+returns:
+- if label 
+- else label
+- end if label
+'''
+def getIfLabel():
+    global ifCount
+    ifLab = "if_{}".format(ifCount)
+    elseLab = "else_{}".format(ifCount)
+    endLab = "endif_{}".format(ifCount)
+    ifCount += 1
+    return ifLab, elseLab, endLab
+
+'''
+#* get previous if labels
+params: -
+returns:
+- previous if label 
+- previous else label
+- previous end if label
+'''
+def getPrevIfLabel():
+    global ifCount
+    ifLab = "if_{}".format(ifCount-1)
+    elseLab = "else_{}".format(ifCount-1)
+    endLab = "endif_{}".format(ifCount-1)
+    ifCount += 1
+    return ifLab, elseLab, endLab
+
+'''
+#* get comparison label
+params: -
+return: comp label
+'''
+def getCompLabel():
+    global compLabelCount
+    compLab = "comp_{}".format(compLabelCount)
+    compLabelCount += 1
+    return compLab
+
+'''
+#* get boolean label
+params: -
+return: bool label
+'''
+def getBoolLabel():
+    global boolLabelCount
+    boolLab = "bool_{}".format(boolLabelCount)
+    boolLabelCount += 1
+    return boolLab
+
+
+'''
+#* make new temporary variable
+params: -
+return:
+- temp (string): new temp variabe name
+'''
+def newTempVar():
+    global tempCount, localTempCount
+    temp = "t{}".format(tempCount)
+    tempCount += 1
+    localTempCount += 1
+    return temp
 
 '''
 #* print 3TAC intermediate code
